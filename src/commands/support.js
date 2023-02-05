302f75485e5d8fae1a97537173ed2a72168729a9
YOUSSEF908 <84159779+YOUSSEF908@users.noreply.github.com>
2023-02-05 20:51:34 +0100

    Update support.js

--- src/commands/support.js
+++ src/commands/support.js
@@ -3,14 +3,25 @@ const config = require("../config")
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("support")
+    .setName("rules")
     .setDescription("Show Enterprise support server."),
     run: async (interaction, client) => {
         
         const embed = new EmbedBuilder()
         .setColor(config.embed.color)
-        .setDescription(`Latecy: ${Date.now() - interaction.createdTimestamp}ms`)
-        
-        await interaction.reply({ content: 'Pong 🏓', embeds: [embed]})       
+        .setDescription(`** ♾️ Ticketing Rules**
+
+ > 1 - +18 / Do not share NSFW content.
+  > 2 - Avoid matters involving harm, damage or death.
+  > 3 - Do not discriminate.
+  > 4 - Do not use channels unnecessarily.
+  > 5 - Do not use spam, floods, unnecessary spoilers or too many capital letters.
+  > 6 - Avoid fights, everyone will be punished, whatever the reason.
+  > 7 - Do not use profanity for anyone.
+ > 8 - Do not tag roles, members or senior people for no reason.
+  > 9 - Do not share anyone's personal information in public.
+
+ **Everyone who joins the server is deemed to have read and accepted the rules!**`)
+        await interaction.reply({ content: '', embeds: [embed]})       
     }
 }
