65e928caadaf5995fc34ed502ce4628f3de3a636
YOUSSEF908 <84159779+YOUSSEF908@users.noreply.github.com>
2023-02-05 18:52:28 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    token: "TOKEN",
-    mongoDb: "MONGODB URL", 
-    clientId: "CLIENT ID",
+    token: "MTA3MTgwNDE2OTU0MDg3ODM4Nw.Gp8ct3.Pj_JIS792QDUSyDM5IvlzWOzl6d_v8NZOYeqY0",
+    mongoDb: "mongodb+srv://shika:01155343499Aa@cluster0.f11tpfg.mongodb.net/spire", 
+    clientId: "1071804169540878387",
     link: {
         supportServer: "SUPPORT SERVER URL",
-        inviteUrl: "INVITE URL",
-        github: "https://github.com/wurathh/ticketprise"
+        inviteUrl: "https://discord.com/api/oauth2/authorize?client_id=1071804169540878387&permissions=3288647744&scope=bot%20applications.commands",
+        github: ""
     },
     embed: {
         color: "#F59838",
