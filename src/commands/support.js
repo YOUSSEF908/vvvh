84c86a4bd7763cbedc96a4c73164cedde66f5d22
YOUSSEF908 <84159779+YOUSSEF908@users.noreply.github.com>
2023-02-15 12:17:17 +0100

    Update support.js

--- src/commands/support.js
+++ src/commands/support.js
@@ -9,10 +9,10 @@ module.exports = {
         
         const embed = new EmbedBuilder()
         .setColor(config.embed.color)
-        .setDescription(`*Get started* using the bot by [inviting](https://discord.com/oauth2/authorize?client_id=1071804169540878387&permissions=8&scope=bot%20applications.commands) the bot to your server.
-- You can use the `/ticket setup` to easily config the bot.
-- if you have any bug or technical issues you can <#1071877395428167700> us.
-- If you have any difficulties while using our bot ask the support for help.`)
+        .setDescription(`Get started using the bot by [inviting](https://discord.com/oauth2/authorize?client_id=1071804169540878387&permissions=8&scope=bot%20applications.commands) the bot to your server.
+You can use the `/ticket setup` to easily config the bot.
+if you have any bug or technical issues you can <#1071877395428167700> us.
+If you have any difficulties while using our bot ask the support for help.`)
         await interaction.reply({ content: '', embeds: [embed]})       
     }
 }
